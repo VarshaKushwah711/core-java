02b2b473a830ce067e8de56bf92f959d45c19c40
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-15 23:22:05 +0530

    Add files via upload

---
+++ _8FinallyKW.java
@@ -0,0 +1,21 @@
+package Exception_handling;
+public class _8FinallyKW {
+    public static void main(String[] args) {
+        try{
+            int arr[] = {1,2,3};
+            System.out.println(arr[0]);
+            System.out.println(arr[1]);
+            System.out.println(arr[2]);
+            System.out.println(arr[3]);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            System.out.println("Finally block always excute either exception occuer or not.");
+        } 
+    }
+}
+//java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
